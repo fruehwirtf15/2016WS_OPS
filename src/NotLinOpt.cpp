b4dbd497f7f8bef055c895e69198b63153bf5296
a8502069@unet.univie.ac.at <fruehwirtf15@univie.ac.at>
2016-12-15 10:45:10 +0100

    Init Main etc

---
+++ src/NotLinOpt.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : NotLinOpt.cpp
+// Author      : Stomakhin Andrey, Fruehwirth Franz, Hanzer Daniel, Cherches Nicole, Filippovits Lukas
+// Version     : 0.1
+// Copyright   : 
+// Description : Nichtlineare Optimierung ohne Nebenbedingungen: Eindimensionale Verfahren
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hashing" << endl; // prints hashing
+	return 0;
+}

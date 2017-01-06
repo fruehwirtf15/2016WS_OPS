90a1bb9eb8d952c857f8427deb3aabfbfb5be91d
a8502069@unet.univie.ac.at <fruehwirtf15@univie.ac.at>
2017-01-06 16:20:03 +0100

    BISEC Methode implementiert

--- src/BI.cpp
+++ src/BI.cpp
@@ -19,11 +19,58 @@ BI::~BI() {
 }
 
 double BI::findMinimum(double a, double b, Funktion &f, double epsilon) {
-	return f.value((a+b)/2.0);
+	double Ratio = 2.0 / 3.0;
+	double interval = (b - a) * epsilon;
+	double lambda, f_lambda;
+	double mue, f_mue;
+
+	lambda = a + (1 - Ratio) * (b - a);
+	f_lambda = f.value(lambda);
+	mue = a + Ratio * (b - a);
+	f_mue = f.value(mue);
+
+	while ((b - a) > interval) {
+		if (f_lambda < f_mue) {
+			b = mue;
+		} else {
+			a = lambda;
+		}
+		mue = a + Ratio * (b - a);
+		f_mue = f.value(mue);
+		lambda = a + (1 - Ratio) * (b - a);
+		f_lambda = f.value(lambda);
+	}
+
+	return (mue - lambda) / 2;
 }
 
-void BI::makeGnuPlotFile(double a, double b, Funktion &f, double epsilon, ofstream& file) {
-	//TODO Ausgabe des Optimierungsverlaufs in eine Datei.
+void BI::makeGnuPlotFile(double a, double b, Funktion &f, double epsilon, ofstream& myfile) {
+	// Ausgabe des Optimierungsverlaufs in eine Datei.
+	double Ratio = 2.0 / 3.0;
+	double interval = (b - a) * epsilon;
+	double lambda, f_lambda;
+	double mue, f_mue;
+	int Iteration = 0;
+
+	lambda = a + (1 - Ratio) * (b - a);
+	f_lambda = f.value(lambda);
+	mue = a + Ratio * (b - a);
+	f_mue = f.value(mue);
+
+	while ((b - a) > interval) {
+		myfile<<Iteration<<";"<<a<<";"<<lambda<<";"<<mue<<";"<<b<<";"<<f_lambda<<";"<<f_mue<<endl;
+		Iteration++;
+		if (f_lambda < f_mue) {
+			b = mue;
+		} else {
+			a = lambda;
+		}
+		mue = a + Ratio * (b - a);
+		f_mue = f.value(mue);
+		lambda = a + (1 - Ratio) * (b - a);
+		f_lambda = f.value(lambda);
+	}
+	myfile<<Iteration<<";"<<a<<";"<<lambda<<";"<<mue<<";"<<b<<";"<<f_lambda<<";"<<f_mue<<endl;
 }
 
 } /* namespace std */
